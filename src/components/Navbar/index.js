5f95d02e626243895c1611f1b8b159a547e85feb
Lorena C <lorena@eunknown.org>
2021-04-17 14:45:01 -0700

    add router

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { Nav } from './NavbarElements';
+import { Nav, NavLink } from './NavbarElements';
 
 const Navbar = () => {
   return (
     <>
-      <Nav>Nav</Nav>
+      <Nav>
+        <NavLink to="/">Foodie</NavLink>
+      </Nav>
     </>
   );
 };
