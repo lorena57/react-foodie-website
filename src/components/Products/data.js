60736d6cd8452d4cf940c2cd335c0ed203c62bc9
Lorena C <lorena@eunknown.org>
2021-04-20 19:07:54 -0700

    add products page

---
+++ src/components/Products/data.js
@@ -0,0 +1,29 @@
+export const productData = [
+  {
+    img: product1,
+    alt: 'Pizza',
+    name: 'Supreme Pizza',
+    desc:
+      'Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto',
+    price: '$19.99',
+    button: 'Add to Cart',
+  },
+  {
+    img: product2,
+    alt: 'Pizza',
+    name: 'Supreme Pizza',
+    desc:
+      'Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto',
+    price: '$19.99',
+    button: 'Add to Cart',
+  },
+  {
+    img: product3,
+    alt: 'Pizza',
+    name: 'Supreme Pizza',
+    desc:
+      'Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto',
+    price: '$19.99',
+    button: 'Add to Cart',
+  },
+];
---
+++ src/components/Products/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const Products = () => {
+  return (
+    <ProductsContainer>
+      <ProductHeading>Heading</ProductHeading>
+      <ProductWrapper></ProductWrapper>
+    </ProductsContainer>
+  );
+};
+
+export default Products;
